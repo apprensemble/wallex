514949ab0b2fc6e5c834d8873e04f74bfa58d6d7
Grillon <thierry.grillon@gmail.com>
2024-08-19 13:35:07 +0200

    update lib version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='myblockscoutlib',
     packages=find_packages(include=['myblockscoutlib']),
-    version='0.2.2',
+    version='0.2.3',
     description='A simple wallet watcher via Blockscout and CMC',
     author='Me',
     install_requires=[],
