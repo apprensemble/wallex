36930714c90b11951ee42457a0d41a992f7d5ce5
Grillon <thierry.grillon@gmail.com>
2024-08-30 14:37:20 +0200

    v0.4.1: ajout dependances

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='wallex',
     packages=find_packages(include=['wallex']),
-    version='0.4.0',
+    version='0.4.1',
     description='A simple wallet watcher via Blockscout,moralis and CMC',
     author='CryptoGrillon',
-    install_requires=[],
+    install_requires=['selenium','requests','typing'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==8.3.2'],
     test_suite='tests'
